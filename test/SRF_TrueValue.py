8824e3f86e85558c263b22ef579c098461185f71
BeichenZhang <zbc555day@163.com>
2021-12-29 23:26:39 +0800

    底图投影使用数据中提供的轨道斜率

--- test/SRF_TrueValue.py
+++ test/SRF_TrueValue.py
@@ -3,6 +3,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import math
+from mpl_toolkits.basemap import Basemap
 
 
 def BRF_TrueValue(o_BRF, scale, offset):
@@ -29,11 +30,11 @@ if __name__ == "__main__":
     BRF_npy_path = r'D:\Work_PhD\MISR_AHI_WS\211204\SRF_62\b_3_c_8.npy'
     BRF_data = numpy.load(BRF_npy_path)
     
-    # original values
-    plt.imshow(BRF_data, cmap=plt.cm.jet, vmin=0, vmax=11000)
-    plt.title('Original values in LandBRF 2016-01-15 Band 4 Camera 9')
-    plt.colorbar()
-    plt.show()
+    # # original values
+    # plt.imshow(BRF_data, cmap=plt.cm.jet, vmin=0, vmax=11000)
+    # plt.title('Original values in LandBRF 2016-01-15 Band 4 Camera 9')
+    # plt.colorbar()
+    # plt.show()
 
     # true values
     scale_landBRF = 0.0001526065170764923
@@ -43,3 +44,35 @@ if __name__ == "__main__":
     plt.title('True values in LandBRF 2016-01-15 Band 4 Camera 9')
     plt.colorbar()
     plt.show()
+            
+    # # mapping
+    # inclination = 1.7157253262878522  # SOM_parameters.som_orbit.i
+    # in_pi = math.pi * inclination
+    # b_lat = 35.616525
+    # b_lon = 140.189875
+    # m = Basemap(
+    #     width=563200,  # row of a block
+    #     height=140800,  # col of a block
+    #     projection='omerc',
+    #     resolution='h',
+    #     rsphere=6378137.0,  # from document
+    #     lat_1=b_lat+in_pi,
+    #     lon_1=b_lon+in_pi,
+    #     lat_2=b_lat-in_pi,
+    #     lon_2=b_lon-in_pi,
+    #     lat_0=b_lat,
+    #     lon_0=b_lon,
+    # )
+    # min_x = 0
+    # min_y = 0
+    # max_x = 563200
+    # max_y = 140800
+    # extent = (min_x, max_x, max_y, min_y)
+
+    # m.imshow(BRF_tv, origin='upper', extent=extent, cmap=plt.cm.jet, vmin=0.0, vmax=1.0)
+    # m.drawcoastlines(color='w', linewidth=0.8)
+    # # m.drawmapboundary(fill_color='whitesmoke')
+    # # m.fillcontinents(color='white', lake_color='whitesmoke', alpha=None)
+    # m.drawparallels(numpy.arange(35., 36.5, 0.5), labels=[1, 0, 0, 0], linewidth=0.1)  # draw parallels
+    # m.drawmeridians(numpy.arange(137., 143., 1.5), labels=[0, 0, 0, 1], linewidth=0.1)  # draw meridians
+    # plt.show()
