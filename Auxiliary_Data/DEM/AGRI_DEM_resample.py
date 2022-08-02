bd6e08fa8e2b94a7dfe46a7b23015027b8544a89
BeichenZhang <zbc555day@163.com>
2022-08-02 21:48:19 +0900

    计算AHI和AGRI需要的DEM的TIFF

---
+++ Auxiliary_Data/DEM/AGRI_DEM_resample.py
@@ -0,0 +1,38 @@
+import os
+import numpy
+
+
+AGRI_EXTENT = ['n60', 'e044', 's60', 'e164']     # lulat, lulon, rblat, rblon
+
+
+def get_dem_tiffs():
+    tif_suffix = '_dem.tif'
+    s_lats = range(60, 0, -5)
+    s_lat_strs = []
+    for s_lat in s_lats:
+        s_lat_str = 's' + (2 - len(str(s_lat))) * '0' + str(s_lat)
+        s_lat_strs.append(s_lat_str)
+    n_lats = range(0, 60, 5)
+    n_lat_strs = []
+    for n_lat in n_lats:
+        n_lat_str = 'n' + (2 - len(str(n_lat))) * '0' + str(n_lat)
+        n_lat_strs.append(n_lat_str)
+    tile_lat_indexs = s_lat_strs + n_lat_strs
+    # print(tile_lat_indexs)
+    e_lons = range(40, 165, 5)
+    e_lon_strs = []
+    for e_lon in e_lons:
+        e_lon_str = 'e' + (3 - len(str(e_lon))) * '0' + str(e_lon)
+        e_lon_strs.append(e_lon_str)
+    tile_lon_indexs = e_lon_strs
+    # print(tile_lon_indexs)
+    tif_tiles_files = []
+    for tile_lat_index in tile_lat_indexs:
+        for tile_lon_index in tile_lon_indexs:
+            tif_tiles_file = tile_lat_index + tile_lon_index + tif_suffix
+            tif_tiles_files.append(tif_tiles_file)
+    print(tif_tiles_files)
+
+
+if __name__ == "__main__":
+    get_dem_tiffs()
