0fd14bc2b4c50aa5a46f9a366469f085a1d8ced1
BeichenZhang <zbc555day@163.com>
2022-09-05 20:32:32 +0900

    使用LUT的AC代码测试完善

--- AHI_AC/ROI_AHI_AC_LUT.py
+++ AHI_AC/ROI_AHI_AC_LUT.py
@@ -9,9 +9,13 @@ from ftplib import FTP
 import xarray as xr
 import subprocess
 import datetime as dt
+import shutil
 
-target = '/data01/people/liwei/Data/AHI_Validation/'
-ws = '/data01/people/beichen/workspace/20220903'
+# workspace
+ws = os.getcwd()
+TEMP_FOLDER = os.path.join(ws, 'temp')
+if not os.path.exists(TEMP_FOLDER):
+    os.makedirs(TEMP_FOLDER)
 
 SZA_PATH = '/data01/GEO/INPUT/ANGLE/Solar_Zenith_Angle_u2/'
 SAA_PATH = '/data01/GEO/INPUT/ANGLE/Solar_Azimuth_Angle_u2/'
@@ -52,17 +56,19 @@ class AHI_angle:
 
         sza_file_name = 'AHI_SZA_2020{}5.dat'.format(AHI_date)
         saa_file_name = 'AHI_SAA_2020{}5.dat'.format(AHI_date)
-        
-        ahi_angle_lats = np.arange(60. - AHI_ANGLE_RESOLUTION / 2, -60, -AHI_ANGLE_RESOLUTION)
-        ahi_angle_lons = np.arange(85. + AHI_ANGLE_RESOLUTION / 2, 205, AHI_ANGLE_RESOLUTION)
+
+        ahi_lats = np.arange(60. - AHI_RESOLUTION / 2, -60, -AHI_RESOLUTION)
+        ahi_lons = np.arange(85. + AHI_RESOLUTION / 2, 205, AHI_RESOLUTION)
 
         with open(SZA_PATH + sza_file_name, 'rb') as fp:
-            AHI_SZA = np.frombuffer(fp.read(), dtype='u2').reshape(3000, 3000)[find_nearest_index(ahi_angle_lats, r_ullat):find_nearest_index(ahi_angle_lats, r_lrlat) + 1, find_nearest_index(ahi_angle_lons, r_ullon):find_nearest_index(ahi_angle_lons, r_lrlon) + 1] / 100
-            AHI_SZA = cv2.resize(np.array(AHI_SZA, dtype='float64'), (row_AHI, col_AHI), interpolation=cv2.INTER_NEAREST)
+            AHI_SZA = np.frombuffer(fp.read(), dtype='u2').reshape(3000, 3000)
+            AHI_SZA = cv2.resize(np.array(AHI_SZA, dtype='float64'), (12000, 12000), interpolation=cv2.INTER_NEAREST)
+            AHI_SZA = AHI_SZA[find_nearest_index(ahi_lats, r_ullat):find_nearest_index(ahi_lats, r_lrlat) + 1, find_nearest_index(ahi_lons, r_ullon):find_nearest_index(ahi_lons, r_lrlon) + 1] / 100
 
         with open(SAA_PATH + saa_file_name, 'rb') as fp:
-            AHI_SAA = np.frombuffer(fp.read(), dtype='u2').reshape(3000, 3000)[find_nearest_index(ahi_angle_lats, r_ullat):find_nearest_index(ahi_angle_lats, r_lrlat) + 1, find_nearest_index(ahi_angle_lons, r_ullon):find_nearest_index(ahi_angle_lons, r_lrlon) + 1] / 100
-            AHI_SAA = cv2.resize(np.array(AHI_SAA, dtype='float64'), (row_AHI, col_AHI), interpolation=cv2.INTER_NEAREST)
+            AHI_SAA = np.frombuffer(fp.read(), dtype='u2').reshape(3000, 3000)
+            AHI_SAA = cv2.resize(np.array(AHI_SAA, dtype='float64'), (12000, 12000), interpolation=cv2.INTER_NEAREST)
+            AHI_SAA = AHI_SAA[find_nearest_index(ahi_lats, r_ullat):find_nearest_index(ahi_lats, r_lrlat) + 1, find_nearest_index(ahi_lons, r_ullon):find_nearest_index(ahi_lons, r_lrlon) + 1] / 100
 
         return AHI_SZA, AHI_SAA
 
@@ -151,12 +157,6 @@ class LUT_interpolation:
 fn1, fn2, fn3 = LUT_interpolation(LUT_PATH).LUT_interpolation()
 
 
-def mkdir(path):
-    folder = os.path.exists(path)
-    if not folder:
-        os.makedirs(path)
-
-
 def Time_split(time):
     YYYY = time.strftime('%Y')
     MM = time.strftime('%m')
@@ -183,7 +183,7 @@ def calculate_6s_band4(i):
     ahi_toa = AHI_data[i, :]
     y = xa * ahi_toa - xb
     SR = y / (1 + xc * y)
-    return ahi_toa, xa, xb, xc, SR
+    return (ahi_toa, xa, xb, xc, SR)
 
 
 def DN2TBB(data):
@@ -199,14 +199,12 @@ def find_nearest_index(array, value):
 
 # main()
 if __name__ == "__main__":
-    roi_name = '0.0_50'
+    roi_name = '0.0_60'
     # roi_extent: (ullat, ullon, lrlat, lrlon)
-    roi_extent = [-3.101, 141.153, -3.281, 141.333]
+    roi_extent = [-13.723, 142.529, -13.899, 142.709]
     # Matched AHI Obs. Time
     ahi_obs_times = [
-        '201701060100', '201711060120', '201711220100', '201712080100', '201712240100', '201801090100', '201811090120', '201811250100', '201812110100', '201812270100', '201901120110', '201911120110', '201911280100', '201912140100',
-        '201912300100', '201704190130', '201705050110', '201705210100', '201706060040', '201706220040', '201707080050', '201707240110', '201708090110', '201804220130', '201805080110', '201805240050', '201806090040', '201806250040',
-        '201807110100', '201807270110', '201808120110', '201904250120', '201905110110', '201905270050', '201906120040', '201906280040', '201907140100', '201907300110', '201908150120'
+        '201704050100', '201704210050', '201708270100', '201709120100', '201804080100', '201808300100', '201809150100', '201904110100', '201908170050', '201909020100'
     ]
     
     roi_ullat = roi_extent[0]
@@ -232,6 +230,10 @@ if __name__ == "__main__":
         AHI_AL[AHI_AL >= max(al)] = max(al) - (1 / 10000)
         AHI_AL[AHI_AL <= min(al)] = min(al) + (1 / 10000)
 
+    ftp = FTP()
+    ftp.connect('hmwr829gr.cr.chiba-u.ac.jp', 21)
+    ftp.login()
+
     for date in ahi_obs_times:
         ahi_data_time = date
         ahi_data_folder1 = ahi_data_time[:6]
@@ -240,105 +242,111 @@ if __name__ == "__main__":
         ahi_saa_path = '/gridded/FD/V20151105/' + ahi_data_folder1 + '/VIS/' + ahi_saa_filename
         ftp_dl_url = 'ftp://hmwr829gr.cr.chiba-u.ac.jp' + ahi_saa_path
         print(ftp_dl_url)
-        ftp = FTP()
-        ftp.connect('hmwr829gr.cr.chiba-u.ac.jp', 21)
-        ftp.login()
-        local_file = ws + '/' + ahi_saa_filename
-        with open(local_file, 'wb') as f:
-            ftp.retrbinary('RETR ' + ahi_saa_path, f.write, 1024 * 1024)
-        p = subprocess.Popen('lbzip2 -d {}'.format(local_file), shell=True)
-        p.communicate()
+        local_file = TEMP_FOLDER + '/' + ahi_saa_filename
+        if not os.path.exists(local_file):
+            with open(local_file, 'wb') as f:
+                ftp.retrbinary('RETR ' + ahi_saa_path, f.write, 1024 * 1024)
+            p = subprocess.Popen('lbzip2 -d {}'.format(local_file), shell=True)
+            p.communicate()
 
     ftp.close()
 
     for ahi_obs_t in ahi_obs_times:
-        start_time = T.time()
-        YYYY, MM, DD, HH, MIN, date = Time_split(ahi_obs_t)
-        print("start processing {}".format(date))
-        # make dir
-        folder_AC = target + date + '_AC/'
-        mkdir(folder_AC)
-        # Download AHI
-        with open(ws + '/{}.vis.03.fld.geoss'.format(date), 'rb') as fp:
-            data = np.frombuffer(fp.read(), dtype='>u2').reshape(12000, 12000)
-            data = DN2TBB(data)
-            data = data / 100
+        try:
+            start_time = T.time()
+            ahi_obs_t_obj = datetime.datetime.strptime(ahi_obs_t, "%Y%m%d%H%M")
+            YYYY, MM, DD, HH, MIN, date = Time_split(ahi_obs_t_obj)
+            print("Start processing {}".format(date))
+            # Download AHI
+            with open(TEMP_FOLDER + '/{}.vis.03.fld.geoss'.format(date), 'rb') as fp:
+                data = np.frombuffer(fp.read(), dtype='>u2').reshape(12000, 12000)
+                data = DN2TBB(data)
+                data = data / 100
 
-        AHI_data = data[find_nearest_index(ahi_lats, roi_ullat):find_nearest_index(ahi_lats, roi_lrlat) + 1, find_nearest_index(ahi_lons, roi_ullon):find_nearest_index(ahi_lons, roi_lrlon) + 1]
+            AHI_data = data[find_nearest_index(ahi_lats, roi_ullat):find_nearest_index(ahi_lats, roi_lrlat) + 1, find_nearest_index(ahi_lons, roi_ullon):find_nearest_index(ahi_lons, roi_lrlon) + 1]
 
-        # Solar angle
-        print('Start reading Angle data')
-        AHI_SZA, AHI_SAA = AHI_angle(date).read_angle_data(roi_extent)
+            # Solar angle
+            print('Start reading Angle data')
+            AHI_SZA, AHI_SAA = AHI_angle(date).read_angle_data(roi_extent)
 
-        RAA = abs(AHI_SAA - AHI_VAA)
-        RAA[RAA > 180] = 360 - RAA[RAA > 180]
+            RAA = abs(AHI_SAA - AHI_VAA)
+            RAA[RAA > 180] = 360 - RAA[RAA > 180]
 
-        print('Angle data read finished')
-        print('Start reading Atmospheric data')
-        OZ, WV, AOT550 = CAMS_data(YYYY, MM, DD, HH, MIN).read_CAMS(n_lats, n_lons)
-        Aerosol_type = CAMS_data(YYYY, MM, DD, HH, MIN).read_CAMS_AERO(n_lats, n_lons)
-        print('Atmospheric data read finished')
+            print('Angle data read finished')
+            print('Start reading Atmospheric data')
+            OZ, WV, AOT550 = CAMS_data(YYYY, MM, DD, HH, MIN).read_CAMS(n_lats, n_lons)
+            Aerosol_type = CAMS_data(YYYY, MM, DD, HH, MIN).read_CAMS_AERO(n_lats, n_lons)
+            print('Atmospheric data read finished')
 
-        AHI_TOA, Xa, Xb, Xc, SR = Parallel(n_jobs=-1)(delayed(calculate_6s_band4)(i) for i in range(row_AHI))
-        # Save file and remove download input data
-        ahi_data_roi = np.array(AHI_TOA).reshape(row_AHI, col_AHI)
-        ac_roi_xa = np.array(Xa).reshape(row_AHI, col_AHI)
-        ac_roi_xb = np.array(Xb).reshape(row_AHI, col_AHI)
-        ac_roi_xc = np.array(Xc).reshape(row_AHI, col_AHI)
-        roi_ahi_sr = np.array(SR).reshape(row_AHI, col_AHI)
+            results = Parallel(n_jobs=-1)(delayed(calculate_6s_band4)(i) for i in range(row_AHI))
+            X = np.array(results)
+            AHI_TOA = X[:, 0]
+            Xa = X[:, 1]
+            Xb = X[:, 2]
+            Xc = X[:, 3]
+            SR = X[:, 4] 
+            # Save file and remove download input data
+            ahi_data_roi = np.array(AHI_TOA).reshape(row_AHI, col_AHI)
+            ac_roi_xa = np.array(Xa).reshape(row_AHI, col_AHI)
+            ac_roi_xb = np.array(Xb).reshape(row_AHI, col_AHI)
+            ac_roi_xc = np.array(Xc).reshape(row_AHI, col_AHI)
+            roi_ahi_sr = np.array(SR).reshape(row_AHI, col_AHI)
 
-        # record AC SR
-        # Template of record
-        #############################
-        # demo = [
-        #     {
-        #         'obs_time': '201608230450',
-        #         'roi_lats': [60.0, 59.99, ..., -60.0],
-        #         'roi_lons': [85.0, 85.01, ..., 205.0],
-        #         'roi_vza': [[..., ..., ...], ...],
-        #         'roi_sza': [[..., ..., ...], ...],
-        #         'roi_raa': [[..., ..., ...], ...],
-        #         'roi_aot': [[..., ..., ...], ...],
-        #         'roi_aero_type': [[..., ..., ...], ...],
-        #         'roi_oz': [[..., ..., ...], ...],
-        #         'roi_wv': [[..., ..., ...], ...],
-        #         'roi_ac_fa': [[..., ..., ...], ...],
-        #         'roi_ac_xa': [[..., ..., ...], ...],
-        #         'roi_ac_xb': [[..., ..., ...], ...],
-        #         'roi_ac_xc': [[..., ..., ...], ...],
-        #         'roi_ahi_data': [[..., ..., ...], ...],
-        #         'roi_ahi_sr': [[..., ..., ...], ...],
-        #     }
-        # ]
-        #############################
-        
-        record_info = [{
-            'obs_time': ahi_obs_t,
-            'roi_lats': n_lats,
-            'roi_lons': n_lons,
-            'roi_vza': AHI_VZA,
-            'roi_sza': AHI_SZA,
-            'roi_raa': RAA,
-            'roi_aot': AOT550,
-            'roi_aero_type': Aerosol_type,
-            'roi_oz': OZ,
-            'roi_wv': WV,
-            'roi_ac_fa': [],
-            'roi_ac_xa': ac_roi_xa,
-            'roi_ac_xb': ac_roi_xb,
-            'roi_ac_xc': ac_roi_xc,
-            'roi_ahi_data': ahi_data_roi,
-            'roi_ahi_sr': roi_ahi_sr,
-        }]
-        
-        folder_path = os.path.join(ws, 'AHI_AC_PARAMETER')
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-        record_npy = os.path.join(folder_path, ahi_obs_t + '_ac_band4.npy')
-        np.save(record_npy, record_info)
-        
-        #                 remove_original_file(folder_original)
-        end_time = T.time()
-        TIME = end_time - start_time
-        print('time: {:.1f} secs, {:.1f} mins,{:.1f} hours'.format(TIME, TIME / 60, TIME / 3600))
-        print("delete file finish")
+            # record AC SR
+            # Template of record
+            #############################
+            # demo = [
+            #     {
+            #         'obs_time': '201608230450',
+            #         'roi_lats': [60.0, 59.99, ..., -60.0],
+            #         'roi_lons': [85.0, 85.01, ..., 205.0],
+            #         'roi_vza': [[..., ..., ...], ...],
+            #         'roi_sza': [[..., ..., ...], ...],
+            #         'roi_raa': [[..., ..., ...], ...],
+            #         'roi_aot': [[..., ..., ...], ...],
+            #         'roi_aero_type': [[..., ..., ...], ...],
+            #         'roi_oz': [[..., ..., ...], ...],
+            #         'roi_wv': [[..., ..., ...], ...],
+            #         'roi_ac_fa': [[..., ..., ...], ...],
+            #         'roi_ac_xa': [[..., ..., ...], ...],
+            #         'roi_ac_xb': [[..., ..., ...], ...],
+            #         'roi_ac_xc': [[..., ..., ...], ...],
+            #         'roi_ahi_data': [[..., ..., ...], ...],
+            #         'roi_ahi_sr': [[..., ..., ...], ...],
+            #     }
+            # ]
+            #############################
+            
+            record_info = [{
+                'obs_time': ahi_obs_t,
+                'roi_lats': n_lats,
+                'roi_lons': n_lons,
+                'roi_vza': AHI_VZA,
+                'roi_sza': AHI_SZA,
+                'roi_raa': RAA,
+                'roi_aot': AOT550,
+                'roi_aero_type': Aerosol_type,
+                'roi_oz': OZ,
+                'roi_wv': WV,
+                'roi_ac_fa': [],
+                'roi_ac_xa': ac_roi_xa,
+                'roi_ac_xb': ac_roi_xb,
+                'roi_ac_xc': ac_roi_xc,
+                'roi_ahi_data': ahi_data_roi,
+                'roi_ahi_sr': roi_ahi_sr,
+            }]
+            
+            folder_path = os.path.join(ws, 'AHI_AC_PARAMETER')
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            record_npy = os.path.join(folder_path, ahi_obs_t + '_ac_band4.npy')
+            np.save(record_npy, record_info)
+
+            end_time = T.time()
+            TIME = end_time - start_time
+            print('time: {:.1f} secs, {:.1f} mins,{:.1f} hours'.format(TIME, TIME / 60, TIME / 3600))
+        except:
+            pass
+
+    shutil.rmtree(TEMP_FOLDER)
+    print("delete folder finish.")
