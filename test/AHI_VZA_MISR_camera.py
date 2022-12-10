161730e8bca30d5fb81b5ffcbdf9f24f82cda2d2
BeichenZhang <zbc555day@163.com>
2022-12-11 00:34:48 +0900

    绘制MISR相机角度在AHI观测范围的位置

---
+++ test/AHI_VZA_MISR_camera.py
@@ -0,0 +1,58 @@
+import numpy
+import matplotlib.pyplot as plt
+
+MISR_VZA = [26.1, 45.6, 60.0, 70.5]
+AHI_SUBPOINT_LON = 140.7
+AHI_SUBPOINT_LON_IDX = round((AHI_SUBPOINT_LON - 85.)/0.04)-1
+AHI_SUBPOINT_LAT_IDX = 1499
+
+
+def find_nearest_index(array, value):
+    array = numpy.asarray(array)
+    idx = (numpy.abs(array - value)).argmin()
+    return idx
+
+
+if __name__ == "__main__":
+    misr_vza_ahi = numpy.zeros((3000, 3000))
+    workspace = r'D:\Work_PhD\MISR_AHI_WS\221210'
+    bin_vza_filename = workspace + '/201706160000.sat.zth.fld.4km.bin'
+
+    misr_camera_AHI_npy = workspace + '/misr_camera_AHI.npy'
+    data_DN = numpy.fromfile(bin_vza_filename, dtype='>f4')
+    data_DN = data_DN.reshape(3000, 3000)
+
+    for lat_idx in range(len(data_DN)):
+        data_row = data_DN[lat_idx]
+        data_row_l = data_row[:AHI_SUBPOINT_LON_IDX+1]
+        data_row_r = data_row[AHI_SUBPOINT_LON_IDX:]
+        for misr_vza in MISR_VZA:
+            l_idx = find_nearest_index(data_row_l, misr_vza)
+            r_idx = find_nearest_index(data_row_r, misr_vza)
+            if l_idx != AHI_SUBPOINT_LON_IDX and l_idx != AHI_SUBPOINT_LON_IDX-1:
+                misr_vza_ahi[lat_idx][l_idx] = 1
+            if r_idx != 0:
+                all_r_idx = r_idx + AHI_SUBPOINT_LON_IDX
+                misr_vza_ahi[lat_idx][all_r_idx] = 1
+
+    data_DN_T = data_DN.T
+    misr_vza_ahi = misr_vza_ahi.T
+    for lat_idx in range(len(data_DN_T)):
+        data_row = data_DN_T[lat_idx]
+        data_row_l = data_row[:AHI_SUBPOINT_LAT_IDX+1]
+        data_row_r = data_row[AHI_SUBPOINT_LAT_IDX:]
+        for misr_vza in MISR_VZA:
+            l_idx = find_nearest_index(data_row_l, misr_vza)
+            r_idx = find_nearest_index(data_row_r, misr_vza)
+            if l_idx != AHI_SUBPOINT_LAT_IDX:
+                misr_vza_ahi[lat_idx][l_idx] = 1
+            if r_idx != 0:
+                all_r_idx = r_idx + AHI_SUBPOINT_LAT_IDX
+                misr_vza_ahi[lat_idx][all_r_idx] = 1
+    misr_vza_ahi = misr_vza_ahi.T
+
+    numpy.save(misr_camera_AHI_npy, misr_vza_ahi)
+
+    plt.imshow(misr_vza_ahi, interpolation=None)
+    plt.colorbar()
+    plt.show()
