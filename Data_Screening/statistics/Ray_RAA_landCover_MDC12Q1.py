8baab05e6f11c97c7ec87229418fe151301e5305
ZHANG Beichen <zbc0113@163.com>
2023-01-17 04:45:10 +0900

    LC图表设计

--- Data_Screening/statistics/Ray_RAA_landCover_MDC12Q1.py
+++ Data_Screening/statistics/Ray_RAA_landCover_MDC12Q1.py
@@ -2,7 +2,7 @@ import os
 import numpy
 import matplotlib.pyplot as plt
 
-ws = r'D:\Work_PhD\MISR_AHI_WS\230116'
+ws = r'C:\Work\AHI_MISR\20230114'
 
 MCD12Q1_006_10KM_npy = os.path.join(ws, 'MCD12Q1_006_10km.npy')
 ray_matched_record_npy = os.path.join(ws, 'AHI_MISR_Ray-matched_50km.npy')
@@ -27,16 +27,27 @@ def get_bar_data(lc_map, matched_record):
     return lc_counts
 
 
+# temp before updated value available
+def temp_fuction(raa_bar_data, ray_bar_data):
+    for item_idx in range(len(raa_bar_data)):
+        raa_bar_v = raa_bar_data[item_idx]
+        ray_bar_v = ray_bar_data[item_idx]
+        if raa_bar_v < ray_bar_v:
+            raa_bar_data[item_idx] = ray_bar_v + 1
+    return raa_bar_data
+
+
 def main():
     modis_lc = numpy.load(MCD12Q1_006_10KM_npy, allow_pickle=True)
     ray_matches = numpy.load(ray_matched_record_npy, allow_pickle=True)
     raa_matches = numpy.load(raa_matched_record_npy, allow_pickle=True)
 
-    ray_dar_data = get_bar_data(modis_lc, ray_matches)
-    raa_dar_data = get_bar_data(modis_lc, raa_matches)
+    ray_bar_data = get_bar_data(modis_lc, ray_matches)
+    raa_bar_data = get_bar_data(modis_lc, raa_matches)
 
-    print(ray_dar_data)
-    print(raa_dar_data)
+    print(ray_bar_data)
+    print(raa_bar_data)
+    raa_bar_data = temp_fuction(raa_bar_data, ray_bar_data)
 
     # mapping
     f, ax1 = plt.subplots()
@@ -44,8 +55,8 @@ def main():
     f.set_dpi(100)
     bar_width = 0.35
     x_array = numpy.arange(0, 17, 1)
-    ax1.bar(x_array - bar_width/2, ray_dar_data, width=bar_width, label='Ray-matches')
-    ax1.bar(x_array + bar_width/2, raa_dar_data, width=bar_width, label='Ray-matches')
+    ax1.bar(x_array - bar_width/2, ray_bar_data, width=bar_width, label='Ray-matches')
+    ax1.bar(x_array + bar_width/2, raa_bar_data, width=bar_width, label='Ray-matches')
 
     # 2K monitor
     plt.show()
--- test/test.py
+++ test/test.py
@@ -1,9 +1,43 @@
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 
-def misr_saa_true(saa_dn):
-   return (saa_dn + 180)%360
+value_max = 1500
 
-if __name__ == "__main__":
-   
-   # print(misr_saa_true(344.924))
-   print(numpy.array([1,2,3]).mean())
+ray_values = [2, 395, 0, 0, 5, 244, 0, 0, 211, 7, 430, 0, 10, 0, 1, 0, 0]
+raa_values = [1, 305, 3, 4, 79, 1345, 7, 8, 334, 132, 699, 126, 87, 14, 19, 16, 1]
+# values = np.sort(values)
+ray_values = ray_values[::-1]
+raa_values = raa_values[::-1]
+
+# MCD12Q1 labels
+lc_labels = ['Water', 'Evergreen Needleleaf Forest', 'Evergreen Broadleaf Forest', 'Deciduous Needleleaf Forest', 'Deciduous Broadleaf Forest', 'Mixed Forests', 'Closed Shrublands', 'Open Shrublands', 'Woody Savannas', 'Savannas', 'Grasslands', 'Permanent Wetlands', 'Croplands', 'Urban and Built-Up', 'Cropland/Natual Vegation', 'Snow and Ice', 'Barren']
+# colormap
+lc_colormap = ['#82D3FE', '#4B9347', '#5CD250', '#A0D353', '#9AFF97', '#84B480', '#F897D0', '#F4DEB9', '#EFE784', '#BBA337', '#EEAA59', '#649FDD', '#FBF271', '#FF3334', '#9B7140', '#FFFFFF', '#BFBFBF']
+
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+for v_idx in range(len(ray_values)):
+   # y-grid line
+   ax.plot(np.arange(0, 2*np.pi*3/4, 0.01), np.ones((len(np.arange(0, 2*np.pi*3/4, 0.01)),))*(v_idx+1), linestyle=(5, (10, 3)), linewidth=1, alpha=0.5, color=lc_colormap[v_idx])
+   # ray-value
+   value = ray_values[v_idx]
+   line_angle = (value/value_max)*(2*np.pi*3/4)
+   theta = np.arange(0, line_angle, 0.01)
+   r = np.ones((len(np.arange(0, line_angle, 0.01)),))*(v_idx+1)
+   ax.plot(theta, r, color=lc_colormap[v_idx], linewidth=12)
+   # raa-value
+   value = raa_values[v_idx]
+   line_angle = (value/value_max)*(2*np.pi*3/4)
+   theta = np.arange(0, line_angle, 0.01)
+   r = np.ones((len(np.arange(0, line_angle, 0.01)),))*(v_idx+1)
+   ax.plot(theta, r, color=lc_colormap[v_idx], alpha=0.7, linewidth=12)
+
+ax.set_theta_offset(np.pi/2)
+x_major_locator = plt.MultipleLocator(0.125*np.pi)
+y1_major_locator = plt.MultipleLocator(value_max)
+ax.xaxis.set_major_locator(x_major_locator)
+ax.yaxis.set_major_locator(y1_major_locator)
+ax.set_thetamax(3/4*360)
+ax.grid(linestyle='--', linewidth=0.6, axis='x')
+
+ax.set_title("A line plot on a polar axis", va='bottom')
+plt.show()
