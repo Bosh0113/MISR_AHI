a912abfcdf5bb1a3a8fa6f4d4552404be436bb1e
BeichenZhang <zbc555day@163.com>
2023-03-03 17:43:32 +0900

    随机取点综合出图

--- AHI-MISR_Inter-com_case/preprocess/05_All_SR_Mapping.py
+++ AHI-MISR_Inter-com_case/preprocess/05_All_SR_Mapping.py
@@ -1,10 +1,113 @@
 import os
 import numpy
 import re
+import random
+from scipy.stats import gaussian_kde, pearsonr
+from sklearn.metrics import mean_squared_error, r2_score
+import math
+import matplotlib.pyplot as plt
 
 WORK_SPACE = os.getcwd()
 
-PIXEL_PAIRS_MAX = 4000
+PIXEL_PAIRS_MAX = 500
+
+
+def identifer(li):
+    result = []
+    for a in li:
+        mean = numpy.nanmean(a)
+        std = numpy.nanstd(a)
+        down = mean - 3 * std
+        up = mean + 3 * std
+        n_a = numpy.where(a < down, numpy.nan, a)
+        n_a = numpy.where(n_a > up, numpy.nan, n_a)
+        result.append(n_a)
+    return result
+
+
+def mapping_scatter(Y, X, figure_title, band_name, axis_min=0.0, axis_max=1.0):
+
+    mapping_folfer = os.path.join(WORK_SPACE, 'figure_scatter')
+    figure_folder = os.path.join(mapping_folfer, str(PIXEL_PAIRS_MAX))
+    if not os.path.exists(figure_folder):
+        os.makedirs(figure_folder)
+    fig_filename = os.path.join(figure_folder, figure_title + '.png')
+
+    # if band_name == 'band3':
+    #     axis_max = 0.5
+
+    fig = plt.figure(figsize=(4, 4))
+    ax1 = fig.add_subplot(111, aspect='equal')
+    ax1.grid(linestyle='--', linewidth=0.3)
+
+    k, b = numpy.polyfit(X, Y, deg=1)
+    rmse = math.sqrt(mean_squared_error(X, Y))
+    N = len(X)
+
+    x = numpy.arange(axis_min, axis_max + 1)
+    y = 1 * x
+
+    xx = numpy.arange(axis_min, axis_max + 0.1, 0.05)
+    yy = k * xx + b
+
+    g_x, g_y = numpy.mgrid[axis_min:axis_max:500j, axis_min:axis_max:500j]
+    positions = numpy.vstack([g_x.ravel(), g_y.ravel()])
+    values = numpy.vstack([X, Y])
+    kernel = gaussian_kde(values)
+    Z = numpy.reshape(kernel(positions).T, g_x.shape)
+
+    ax1.minorticks_on()
+    # x_major_locator = plt.MultipleLocator(5)
+    x_minor_locator = plt.MultipleLocator(0.05)
+    ax1.xaxis.set_minor_locator(x_minor_locator)
+    # ax.xaxis.set_major_locator(x_major_locator)
+    ax1.yaxis.set_minor_locator(x_minor_locator)
+    # ax.yaxis.set_major_locator(x_major_locator)
+
+    ax1.tick_params(axis="y", which='minor', length=5, direction='in', labelsize=8)
+    ax1.tick_params(axis="y", which='major', length=10, direction='in', labelsize=8)
+
+    ax1.tick_params(axis="x", which='minor', length=5, direction='in', labelsize=8)
+    ax1.tick_params(axis="x", which='major', length=10, direction='in', labelsize=8)
+
+    ax1.spines['right'].set_color('none')
+    ax1.spines['top'].set_color('none')
+
+    ax1.set_xticks(numpy.arange(axis_min, axis_max + 0.1, 0.1))
+    ax1.set_yticks(numpy.arange(axis_min + 0.1, axis_max + 0.1, 0.1))
+
+    band_label = {
+        'band3': 'Band3',
+        'band4': 'Band4',
+    }
+
+    ax1.set_ylabel("AHI Surface Reflectance " + band_label[band_name], fontsize=12)
+    ax1.set_xlabel("MISR Surface Reflectance " + band_label[band_name], fontsize=12)
+
+    ax1.imshow(numpy.rot90(Z), cmap=plt.cm.gist_earth_r, extent=[axis_min, axis_max, axis_min, axis_max], alpha=0.8, zorder=0)
+    ax1.plot(x, y, color='k', linewidth=1.2, linestyle='-.', zorder=1)
+    ax1.plot(xx, yy, color='r', linewidth=1, linestyle='-', zorder=2)
+    ax1.plot(X, Y, 'k.', markersize=0.5, alpha=0.8, zorder=4)
+
+    text_x = axis_min + (axis_max - axis_min) * 0.07
+    text_y = axis_max - (axis_max - axis_min) * 0.25
+
+    r_, p = pearsonr(X, Y)
+    p_str = '%.3e' % p
+    
+    label_str = label_str = 'y = {}x + {}\nRMSE = {}\nr = {}\n'.format(round(k, 2), round(b, 2), round(rmse, 3), round(r_, 2))
+    if b < 0:
+        label_str = label_str = 'y = {}x - {}\nRMSE = {}\nr = {}\n'.format(round(k, 2), abs(round(b, 2)), round(rmse, 3), round(r_, 2))
+
+    ax1.text(text_x, text_y, s=label_str, fontsize=12)
+
+    ax1.set_xlim(axis_min, axis_max)
+    ax1.set_ylim(axis_min, axis_max)
+    fig.savefig(fig_filename, dpi=1000, bbox_inches='tight')
+    print(fig_filename)
+    plt.close(fig)
+    plt.clf()
+
 
 if __name__ == "__main__":
 
@@ -55,5 +158,26 @@ if __name__ == "__main__":
                         misr_SR_band4_item_list.extend(roi_misr_SR_band4_list)
                         ahi_SR_band4_item_list.extend(roi_ahi_SR_band4_list)
 
+            print('Random NO.:', PIXEL_PAIRS_MAX)
             print(folder_l1, folder_l2)
+            print('MISR_SR_Band3_NO.', 'AHI_SR_Band3_NO.', 'MISR_SR_Band4_NO.', 'AHI_SR_Band4_NO.')
             print(len(misr_SR_band3_item_list), len(ahi_SR_band3_item_list), len(misr_SR_band4_item_list), len(ahi_SR_band4_item_list))
+
+            if len(misr_SR_band3_item_list) > PIXEL_PAIRS_MAX:
+                # mapping
+                index_array = random.sample([idx for idx in range(len(misr_SR_band3_item_list))], PIXEL_PAIRS_MAX)
+                index_array = numpy.sort(index_array).tolist()
+
+                misr_SR_band3_pts = numpy.array(misr_SR_band3_item_list)
+                show_misr_sr_b3 = misr_SR_band3_pts[index_array]
+                ahi_SR_band3_pts = numpy.array(ahi_SR_band3_item_list)
+                show_ahi_sr_b3 = ahi_SR_band3_pts[index_array]
+                figure_title = folder_l1 + '_' + folder_l2 + '_b3' + '_' + str(PIXEL_PAIRS_MAX)
+                mapping_scatter(show_ahi_sr_b3, show_misr_sr_b3, figure_title, 'band3', axis_min=0.0, axis_max=1.0)
+
+                misr_SR_band4_pts = numpy.array(misr_SR_band4_item_list)
+                show_misr_sr_b4 = misr_SR_band4_pts[index_array]
+                ahi_SR_band4_pts = numpy.array(ahi_SR_band4_item_list)
+                show_ahi_sr_b4 = ahi_SR_band4_pts[index_array]
+                figure_title = folder_l1 + '_' + folder_l2 + '_b4' + '_' + str(PIXEL_PAIRS_MAX)
+                mapping_scatter(show_ahi_sr_b4, show_misr_sr_b4, figure_title, 'band4', axis_min=0.0, axis_max=1.0)
