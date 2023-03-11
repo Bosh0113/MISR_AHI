266effd433bfd7c2e8cce91666f97020efed1bc4
ZHANG Beichen <zbc0113@163.com>
2023-03-11 15:48:52 +0900

    继上
    去掉数据处理的bug

--- AHI-MISR_Inter-com_case/statistics/REF_SZA_VZA_variation.py
+++ AHI-MISR_Inter-com_case/statistics/REF_SZA_VZA_variation.py
@@ -4,6 +4,8 @@ import re
 
 WORK_SPACE = os.getcwd()
 
+DEGREE_INTERNAL = 1
+
 
 def find_nearest_index(array, value):
     array = numpy.asarray(array)
@@ -13,6 +15,7 @@ def find_nearest_index(array, value):
 
 if __name__ == "__main__":
     folder_l1_list = ['26', '45', '60', '70']
+    folder_l1_list = ['26']
     folder_l2_list = ['0']
 
     ws_folder = os.path.join(WORK_SPACE, 'ref_sza_vza_variation')
@@ -20,7 +23,7 @@ if __name__ == "__main__":
         os.makedirs(ws_folder)
 
     record_str = ''
-    refer_sza_idx = numpy.arange(15, 80, 0.5)
+    refer_sza_idx = numpy.arange(15, 80, DEGREE_INTERNAL)
     for folder_l1 in folder_l1_list:
         folder_l1_path = os.path.join(WORK_SPACE, folder_l1)
         for folder_l2 in folder_l2_list:
@@ -33,7 +36,7 @@ if __name__ == "__main__":
             v_ahi_toa_record = []
             v_ahi_sr_record = []
             v_misr_sr_record = []
-            for i in range((80-15)*2):  # 0.5 internal
+            for i in range((80-15)*int(1/DEGREE_INTERNAL)):
                 v_ahi_toa_record.append([])
                 v_ahi_sr_record.append([])
                 v_misr_sr_record.append([])
@@ -45,94 +48,106 @@ if __name__ == "__main__":
                 roi_folder_path = os.path.join(folder_l2_path, roi_folder)
                 roi_infos = roi_folder.split('_')
                 roi_lc_idx = roi_infos[1]   # land cover idx
-                ac_record_path = os.path.join(roi_folder_path, 'AHI_AC_PARAMETER')
-                if os.path.exists(ac_record_path):
-                    ac_list = os.listdir(ac_record_path)
-                    for ac_npy in ac_list:
-                        band_re = r'(\S+)' + band_label + '.npy'
-                        ac_matchObj = re.search(band_re, ac_npy)
-                        if ac_matchObj:
-                            ac_npy_path = os.path.join(ac_record_path, ac_npy)
-                            ac_record = numpy.load(ac_npy_path, allow_pickle=True)[0]
-                            obs_time = ac_record['obs_time']
-                            roi_vza_array = ac_record['roi_vza']
-                            roi_vza = round(roi_vza_array.mean(), 3)
-                            roi_sza_array = ac_record['roi_sza']
-                            roi_sza = round(roi_sza_array.mean(), 3)
-                            roi_raa_array = ac_record['roi_raa']
-                            roi_raa = round(roi_raa_array.mean(), 3)
+                if roi_lc_idx == '7':   # Open Shrublands
+                    ac_record_path = os.path.join(roi_folder_path, 'AHI_AC_PARAMETER')
+                    if os.path.exists(ac_record_path):
+                        ac_list = os.listdir(ac_record_path)
+                        for ac_npy in ac_list:
+                            band_re = r'(\S+)' + band_label + '.npy'
+                            ac_matchObj = re.search(band_re, ac_npy)
+                            if ac_matchObj:
+                                ac_npy_path = os.path.join(ac_record_path, ac_npy)
+                                ac_record = numpy.load(ac_npy_path, allow_pickle=True)[0]
+                                obs_time = ac_record['obs_time']
+                                roi_vza_array = ac_record['roi_vza']
+                                roi_vza = round(roi_vza_array.mean(), 3)
+                                roi_sza_array = ac_record['roi_sza']
+                                roi_sza = round(roi_sza_array.mean(), 3)
+                                roi_raa_array = ac_record['roi_raa']
+                                roi_raa = round(roi_raa_array.mean(), 3)
 
-                #             if roi_sza > max_sza:
-                #                 max_sza = roi_sza
-                #             if roi_sza < min_sza:
-                #                 min_sza = roi_sza
-                #             if roi_raa > max_raa:
-                #                 max_raa = roi_raa
-                #             if roi_raa < min_raa:
-                #                 min_raa = roi_raa
-                            if roi_raa > 25. or roi_raa < 35.:
-                                # get v: AHI-TOA MISR-SR AHI-SR
-                                # AHI-TOA
-                                roi_toa_array = ac_record['roi_ahi_data']
-                                v_ahi_toa = round(numpy.array(roi_toa_array).mean(), 3)
-                                # AHI-SR MISR-SR
-                                roi_file_list = os.listdir(roi_folder_path)
-                                v_re = obs_time + '_' + band_label + r'_(\d+).npy'
-                                v_ahi_sr = None
-                                v_misr_sr = None
-                                for roi_file in roi_file_list:
-                                    v_matchObj = re.search(v_re, roi_file)
-                                    if v_matchObj:
-                                        v_record_npy = os.path.join(roi_folder_path, roi_file)
-                                        v_record = numpy.load(v_record_npy, allow_pickle=True)[0]
-                                        roi_ahi_sr = v_record['ahi_sr2misr']
-                                        v_ahi_sr = round(numpy.array(roi_ahi_sr).mean(), 3)
-                                        roi_misr_sr = v_record['misr_v3']
-                                        v_misr_sr = round(numpy.array(roi_misr_sr).mean(), 3)
-                                        break
+                    #             if roi_sza > max_sza:
+                    #                 max_sza = roi_sza
+                    #             if roi_sza < min_sza:
+                    #                 min_sza = roi_sza
+                    #             if roi_raa > max_raa:
+                    #                 max_raa = roi_raa
+                    #             if roi_raa < min_raa:
+                    #                 min_raa = roi_raa
+                                if roi_raa > 25. or roi_raa < 35.:
+                                    # get v: AHI-TOA MISR-SR AHI-SR
+                                    # AHI-TOA
+                                    roi_toa_array = ac_record['roi_ahi_data']
+                                    # AHI-SR MISR-SR
+                                    roi_file_list = os.listdir(roi_folder_path)
+                                    v_re = obs_time + '_' + band_label + r'_(\d+).npy'
+                                    v_ahi_toa = None
+                                    v_ahi_sr = None
+                                    v_misr_sr = None
+                                    for roi_file in roi_file_list:
+                                        v_matchObj = re.search(v_re, roi_file)
+                                        if v_matchObj:
+                                            v_record_npy = os.path.join(roi_folder_path, roi_file)
+                                            v_record = numpy.load(v_record_npy, allow_pickle=True)[0]
+                                            roi_ahi_sr = numpy.array(v_record['ahi_sr2misr']).flatten()
+                                            v_ahi_sr = roi_ahi_sr[~numpy.isnan(roi_ahi_sr)]
+                                            # v_ahi_sr = round(roi_ahi_sr.mean(), 3)
+                                            roi_misr_sr_array = numpy.array(v_record['misr_v3'])
+                                            roi_misr_sr = roi_misr_sr_array.flatten()
+                                            v_misr_sr = roi_misr_sr[~numpy.isnan(roi_misr_sr)]
+                                            # v_misr_sr = round(roi_misr_sr.mean(), 3)
+                                            mask_array = numpy.zeros_like(roi_misr_sr_array)
+                                            mask_array[roi_misr_sr_array > 0.0] = 1.
+                                            mask_array[mask_array == 0.0] = numpy.NaN
+                                            roi_toa_array = roi_toa_array * mask_array
+                                            roi_ahi_toa = roi_toa_array.flatten()
+                                            v_ahi_toa = roi_ahi_toa[~numpy.isnan(roi_ahi_toa)]
+                                            # v_ahi_toa = round(numpy.array(roi_ahi_toa).mean(), 3)
+                                            break
 
-                                rec_idx = find_nearest_index(refer_sza_idx, roi_sza)
-                                # Value y
-                                if (v_ahi_sr is not None) and (v_misr_sr is not None):
-                                    v_ahi_toa_record_sza = v_ahi_toa_record[rec_idx]
-                                    v_ahi_toa_record_sza.append(v_ahi_toa)
-                                    v_ahi_toa_record[rec_idx] = v_ahi_toa_record_sza
-                                    v_ahi_sr_record_sza = v_ahi_sr_record[rec_idx]
-                                    v_ahi_sr_record_sza.append(v_ahi_sr)
-                                    v_ahi_sr_record[rec_idx] = v_ahi_sr_record_sza
-                                    v_misr_sr_record_sza = v_misr_sr_record[rec_idx]
-                                    v_misr_sr_record_sza.append(v_misr_sr)
-                                    v_misr_sr_record[rec_idx] = v_misr_sr_record_sza
+                                    rec_idx = find_nearest_index(refer_sza_idx, roi_sza)
+                                    # Value y
+                                    if (v_ahi_sr is not None) and (v_misr_sr is not None):
+                                        v_ahi_toa_record_sza = v_ahi_toa_record[rec_idx]
+                                        v_ahi_toa_record_sza = numpy.append(v_ahi_toa_record_sza, v_ahi_toa)
+                                        v_ahi_toa_record[rec_idx] = v_ahi_toa_record_sza
+                                        v_ahi_sr_record_sza = v_ahi_sr_record[rec_idx]
+                                        v_ahi_sr_record_sza = numpy.append(v_ahi_sr_record_sza, v_ahi_sr)
+                                        v_ahi_sr_record[rec_idx] = v_ahi_sr_record_sza
+                                        v_misr_sr_record_sza = v_misr_sr_record[rec_idx]
+                                        v_misr_sr_record_sza = numpy.append(v_misr_sr_record_sza, v_misr_sr)
+                                        v_misr_sr_record[rec_idx] = v_misr_sr_record_sza
+            numpy.save(os.path.join(ws_folder, folder_l1 + '_' + folder_l2 + '_' + band_label + '_ref_sza_vza_variation.npy'), [v_ahi_toa_record, v_ahi_sr_record, v_misr_sr_record])
 
-            y_v_ahi_toa = numpy.zeros_like(refer_sza_idx)
-            y_v_ahi_sr = numpy.zeros_like(refer_sza_idx)
-            y_v_misr_sr = numpy.zeros_like(refer_sza_idx)
-            record_str += folder_l1 + '_' + folder_l2 + '_' + band_label + '\n'
-            v_ahi_toa_str =''
-            v_ahi_sr_str =''
-            v_misr_sr_str =''
-            for v_item_idx in range(len(v_ahi_toa_record)):
-                v_ahi_toa_item = v_ahi_toa_record[v_item_idx]
-                v_ahi_toa_mean = round(numpy.array(v_ahi_toa_item).mean(), 3)
-                y_v_ahi_toa[v_item_idx] = v_ahi_toa_mean
-                v_ahi_toa_str += str(v_ahi_toa_mean) + '\t'
-                v_ahi_sr_item = v_ahi_sr_record[v_item_idx]
-                v_ahi_sr_mean = round(numpy.array(v_ahi_sr_item).mean(), 3)
-                y_v_ahi_sr[v_item_idx] = v_ahi_sr_mean
-                v_ahi_sr_str += str(v_ahi_sr_mean) + '\t'
-                v_misr_sr_item = v_misr_sr_record[v_item_idx]
-                v_misr_sr_mean = round(numpy.array(v_misr_sr_item).mean(), 3)
-                y_v_misr_sr[v_item_idx] = v_misr_sr_mean
-                v_misr_sr_str += str(v_misr_sr_mean) + '\t'
+    #         y_v_ahi_toa = numpy.zeros_like(refer_sza_idx)
+    #         y_v_ahi_sr = numpy.zeros_like(refer_sza_idx)
+    #         y_v_misr_sr = numpy.zeros_like(refer_sza_idx)
+    #         record_str += folder_l1 + '_' + folder_l2 + '_' + band_label + '\n'
+    #         v_ahi_toa_str =''
+    #         v_ahi_sr_str =''
+    #         v_misr_sr_str =''
+    #         for v_item_idx in range(len(v_ahi_toa_record)):
+    #             v_ahi_toa_item = v_ahi_toa_record[v_item_idx]
+    #             v_ahi_toa_mean = round(numpy.median(v_ahi_toa_item), 3)
+    #             y_v_ahi_toa[v_item_idx] = v_ahi_toa_mean
+    #             v_ahi_toa_str += str(v_ahi_toa_mean) + '\t'
+    #             v_ahi_sr_item = v_ahi_sr_record[v_item_idx]
+    #             v_ahi_sr_mean = round(numpy.median(v_ahi_sr_item), 3)
+    #             y_v_ahi_sr[v_item_idx] = v_ahi_sr_mean
+    #             v_ahi_sr_str += str(v_ahi_sr_mean) + '\t'
+    #             v_misr_sr_item = v_misr_sr_record[v_item_idx]
+    #             v_misr_sr_mean = round(numpy.median(v_misr_sr_item), 3)
+    #             y_v_misr_sr[v_item_idx] = v_misr_sr_mean
+    #             v_misr_sr_str += str(v_misr_sr_mean) + '\t'
             
-            record_str += v_ahi_toa_str + '\n' + v_ahi_sr_str + '\n' + v_misr_sr_str + '\n'
-            print(y_v_ahi_toa)
-            print(y_v_ahi_sr)
-            print(y_v_misr_sr)
-            numpy.save(os.path.join(ws_folder, folder_l1 + '_' + folder_l2 + '_' + band_label + '_ref_sza_vza_variation.npy'), numpy.array([y_v_ahi_toa, y_v_ahi_sr, y_v_misr_sr]))
+    #         record_str += v_ahi_toa_str + '\n' + v_ahi_sr_str + '\n' + v_misr_sr_str + '\n'
+    #         print(y_v_ahi_toa)
+    #         print(y_v_ahi_sr)
+    #         print(y_v_misr_sr)
+    #         numpy.save(os.path.join(ws_folder, folder_l1 + '_' + folder_l2 + '_' + band_label + '_ref_sza_vza_variation.npy'), numpy.array([y_v_ahi_toa, y_v_ahi_sr, y_v_misr_sr]))
 
-            # print('sza', min_sza, max_sza)
-            # print('raa', min_raa, max_raa)
-    # save result as txt
-    with open(os.path.join(ws_folder, 'ref_sza_vza_variation.txt'), 'w') as f:
-        f.write(record_str)
+    #         # print('sza', min_sza, max_sza)
+    #         # print('raa', min_raa, max_raa)
+    # # save result as txt
+    # with open(os.path.join(ws_folder, 'ref_sza_vza_variation.txt'), 'w') as f:
+    #     f.write(record_str)
--- AHI-MISR_Inter-com_case/statistics/demo/ref_sza_vaa_mapping.py
+++ AHI-MISR_Inter-com_case/statistics/demo/ref_sza_vaa_mapping.py
@@ -1,16 +1,55 @@
 import numpy
-import matplotlib
 import matplotlib.pyplot as plt
 
-deme_npy = r'E:\MISR_AHI_WS\230310\26_0_band3_ref_sza_vza_variation.npy'
+deme_npy = r'E:\MISR_AHI_WS\230310\box_plot\26_0_band3_ref_sza_vza_variation.npy'
+
+DEGREE_INTERNAL = 1
+
+
+def line_plot(all_data):
+    v_ahi_toa_record = all_data[0]
+    v_ahi_sr_record = all_data[1]
+    v_misr_sr_record = all_data[2]
+    refer_sza_idx = numpy.arange(15, 80, DEGREE_INTERNAL)
+    y_v_ahi_toa = numpy.zeros_like(refer_sza_idx)*1.
+    y_v_ahi_sr = numpy.zeros_like(refer_sza_idx)*1.
+    y_v_misr_sr = numpy.zeros_like(refer_sza_idx)*1.
+    for v_item_idx in range(len(v_ahi_toa_record)):
+        v_ahi_toa_item = v_ahi_toa_record[v_item_idx]
+        if len(v_ahi_toa_item) > 0:
+            v_ahi_toa_mean = round(numpy.median(v_ahi_toa_item), 3)
+            print(v_ahi_toa_mean)
+            y_v_ahi_toa[v_item_idx] = v_ahi_toa_mean
+            print(y_v_ahi_toa[v_item_idx])
+            v_ahi_sr_item = v_ahi_sr_record[v_item_idx]
+            v_ahi_sr_mean = round(numpy.median(v_ahi_sr_item), 3)
+            y_v_ahi_sr[v_item_idx] = v_ahi_sr_mean
+            v_misr_sr_item = v_misr_sr_record[v_item_idx]
+            v_misr_sr_mean = round(numpy.median(v_misr_sr_item), 3)
+            y_v_misr_sr[v_item_idx] = v_misr_sr_mean
+
+    y_v_ahi_toa[y_v_ahi_toa==0.] = numpy.NaN
+    y_v_ahi_sr[y_v_ahi_sr==0.] = numpy.NaN
+    y_v_misr_sr[y_v_misr_sr==0.] = numpy.NaN
+    f, ax = plt.subplots()
+    print(y_v_ahi_toa)
+    print(y_v_ahi_sr)
+    print(y_v_misr_sr)
+    ax.plot([i for i in range((80-15)*int(1/DEGREE_INTERNAL))], y_v_ahi_toa, label='AHI TOA')
+    ax.plot([i for i in range((80-15)*int(1/DEGREE_INTERNAL))], y_v_ahi_sr, label='AHI SR')
+    ax.plot([i for i in range((80-15)*int(1/DEGREE_INTERNAL))], y_v_misr_sr, label='MISR SR')
+    plt.legend()
+    plt.show()
+
+
+def box_plot(all_data):
+    fig, axs = plt.subplots()
+    axs.boxplot(all_data)
+    axs.set_title('Box plot')
+    plt.show()
 
 
 if __name__ == "__main__":
     npy_array = numpy.load(deme_npy, allow_pickle=True)
-
-    f, ax = plt.subplots()
-    ax.plot([i for i in range((80-15)*2)], npy_array[0], label='AHI TOA')
-    ax.plot([i for i in range((80-15)*2)], npy_array[1], label='AHI SR')
-    ax.plot([i for i in range((80-15)*2)], npy_array[2], label='MISR SR')
-    plt.legend()
-    plt.show()
+    line_plot(npy_array)
+    # box_plot(npy_array[2])
