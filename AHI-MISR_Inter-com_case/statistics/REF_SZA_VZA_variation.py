52338ffed201546a6114cded6847d6c3801e2c86
ZHANG Beichen <zbc0113@163.com>
2023-03-12 00:28:36 +0900

    继上 完成出图

--- AHI-MISR_Inter-com_case/statistics/REF_SZA_VZA_variation.py
+++ AHI-MISR_Inter-com_case/statistics/REF_SZA_VZA_variation.py
@@ -1,6 +1,8 @@
 import os
 import numpy
 import re
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
 
 WORK_SPACE = os.getcwd()
 
@@ -13,8 +15,84 @@ def find_nearest_index(array, value):
     return idx
 
 
+def box_plot(all_data):
+    fig, axs = plt.subplots()
+    axs.grid(linestyle='--', linewidth=0.3)
+
+    colors_map = ['green', 'blue', 'red']
+    label_list = ['MISR SR', 'AHI TOA', 'AHI SR']
+    for idx in range(len(all_data)):
+        axs.boxplot(all_data[idx],
+                    patch_artist=True,
+                    showfliers=False,
+                    boxprops=dict(facecolor=colors_map[idx],color=colors_map[idx], linewidth=0.5, alpha=0.2),
+                    medianprops=dict(color=colors_map[idx], linewidth=1),
+                    whiskerprops=dict(color=colors_map[idx], linewidth=0.5, alpha=0.3),
+                    capprops=dict(color=colors_map[idx], linewidth=0.5, alpha=0.3))
+    
+    v_ahi_toa_record = all_data[0]
+    v_ahi_sr_record = all_data[1]
+    v_misr_sr_record = all_data[2]
+    refer_sza_idx = numpy.arange(15, 80, DEGREE_INTERNAL)
+    y_v_ahi_toa = numpy.zeros_like(refer_sza_idx)*1.
+    y_v_ahi_sr = numpy.zeros_like(refer_sza_idx)*1.
+    y_v_misr_sr = numpy.zeros_like(refer_sza_idx)*1.
+    for v_item_idx in range(len(v_ahi_toa_record)):
+        v_ahi_toa_item = v_ahi_toa_record[v_item_idx]
+        if len(v_ahi_toa_item) > 0:
+            v_ahi_toa_mean = round(numpy.median(v_ahi_toa_item), 3)
+            y_v_ahi_toa[v_item_idx] = v_ahi_toa_mean
+            v_ahi_sr_item = v_ahi_sr_record[v_item_idx]
+            v_ahi_sr_mean = round(numpy.median(v_ahi_sr_item), 3)
+            y_v_ahi_sr[v_item_idx] = v_ahi_sr_mean
+            v_misr_sr_item = v_misr_sr_record[v_item_idx]
+            v_misr_sr_mean = round(numpy.median(v_misr_sr_item), 3)
+            y_v_misr_sr[v_item_idx] = v_misr_sr_mean
+
+    y_v_ahi_toa[y_v_ahi_toa==0.] = numpy.NaN
+    y_v_ahi_sr[y_v_ahi_sr==0.] = numpy.NaN
+    y_v_misr_sr[y_v_misr_sr==0.] = numpy.NaN
+
+    y_zip = [y_v_ahi_toa, y_v_ahi_sr, y_v_misr_sr]
+    x_range = numpy.array([i for i in range((80-15)*int(1/DEGREE_INTERNAL))])
+    for y_idx in range(len(y_zip)):
+        y_data = y_zip[y_idx]
+        mask_idx = ~numpy.isnan(y_data)
+        v_slope, v_offset, v_r, v_p, v_std_err = linregress(x_range[mask_idx], y_data[mask_idx])
+        print(v_slope, v_offset)
+        y = v_slope*x_range+v_offset
+        plt.plot(x_range, y, '--', color=colors_map[y_idx], label=label_list[y_idx], linewidth=0.8, alpha=0.5)
+
+    axs.minorticks_on()
+    x_minor_locator = plt.MultipleLocator(1)
+    x_major_locator = plt.MultipleLocator(5)
+    axs.xaxis.set_minor_locator(x_minor_locator)
+    axs.xaxis.set_major_locator(x_major_locator)
+    y_minor_locator = plt.MultipleLocator(0.01)
+    y_major_locator = plt.MultipleLocator(0.05)
+    axs.yaxis.set_minor_locator(y_minor_locator)
+    axs.yaxis.set_major_locator(y_major_locator)
+
+    axs.spines['right'].set_color('none')
+    axs.spines['top'].set_color('none')
+
+    axs.tick_params(axis="x", which='minor', length=5, direction='in', labelsize=15)
+    axs.tick_params(axis="x", which='major', length=5, direction='in', labelsize=15)
+    axs.tick_params(axis="y", which='minor', length=5, direction='out', labelsize=15)
+    axs.tick_params(axis="y", which='major', length=5, direction='out', labelsize=15)
+
+    plt.xticks([i for i in range(0, (80-15), 5*int(1/DEGREE_INTERNAL))], numpy.arange(15, 80, 5*int(1/DEGREE_INTERNAL)))
+    plt.xlim(5, 37.5)
+    plt.ylim(0.02, 0.375)
+    plt.xlabel('SZA (°), VZA ≈ 26°, RAA ≈ 30°', size=18)
+    plt.ylabel('Reflectance', size=18)
+    plt.legend(markerscale=2, loc=2, fontsize='x-large')
+    plt.show()
+    plt.clf()
+
+
 if __name__ == "__main__":
-    folder_l1_list = ['26', '45', '60', '70']
+    # folder_l1_list = ['26', '45', '60', '70']
     folder_l1_list = ['26']
     folder_l2_list = ['0']
 
@@ -117,37 +195,6 @@ if __name__ == "__main__":
                                         v_misr_sr_record_sza = v_misr_sr_record[rec_idx]
                                         v_misr_sr_record_sza = numpy.append(v_misr_sr_record_sza, v_misr_sr)
                                         v_misr_sr_record[rec_idx] = v_misr_sr_record_sza
-            numpy.save(os.path.join(ws_folder, folder_l1 + '_' + folder_l2 + '_' + band_label + '_ref_sza_vza_variation.npy'), [v_ahi_toa_record, v_ahi_sr_record, v_misr_sr_record])
-
-    #         y_v_ahi_toa = numpy.zeros_like(refer_sza_idx)
-    #         y_v_ahi_sr = numpy.zeros_like(refer_sza_idx)
-    #         y_v_misr_sr = numpy.zeros_like(refer_sza_idx)
-    #         record_str += folder_l1 + '_' + folder_l2 + '_' + band_label + '\n'
-    #         v_ahi_toa_str =''
-    #         v_ahi_sr_str =''
-    #         v_misr_sr_str =''
-    #         for v_item_idx in range(len(v_ahi_toa_record)):
-    #             v_ahi_toa_item = v_ahi_toa_record[v_item_idx]
-    #             v_ahi_toa_mean = round(numpy.median(v_ahi_toa_item), 3)
-    #             y_v_ahi_toa[v_item_idx] = v_ahi_toa_mean
-    #             v_ahi_toa_str += str(v_ahi_toa_mean) + '\t'
-    #             v_ahi_sr_item = v_ahi_sr_record[v_item_idx]
-    #             v_ahi_sr_mean = round(numpy.median(v_ahi_sr_item), 3)
-    #             y_v_ahi_sr[v_item_idx] = v_ahi_sr_mean
-    #             v_ahi_sr_str += str(v_ahi_sr_mean) + '\t'
-    #             v_misr_sr_item = v_misr_sr_record[v_item_idx]
-    #             v_misr_sr_mean = round(numpy.median(v_misr_sr_item), 3)
-    #             y_v_misr_sr[v_item_idx] = v_misr_sr_mean
-    #             v_misr_sr_str += str(v_misr_sr_mean) + '\t'
-            
-    #         record_str += v_ahi_toa_str + '\n' + v_ahi_sr_str + '\n' + v_misr_sr_str + '\n'
-    #         print(y_v_ahi_toa)
-    #         print(y_v_ahi_sr)
-    #         print(y_v_misr_sr)
-    #         numpy.save(os.path.join(ws_folder, folder_l1 + '_' + folder_l2 + '_' + band_label + '_ref_sza_vza_variation.npy'), numpy.array([y_v_ahi_toa, y_v_ahi_sr, y_v_misr_sr]))
-
-    #         # print('sza', min_sza, max_sza)
-    #         # print('raa', min_raa, max_raa)
-    # # save result as txt
-    # with open(os.path.join(ws_folder, 'ref_sza_vza_variation.txt'), 'w') as f:
-    #     f.write(record_str)
+            numpy.save(os.path.join(ws_folder, folder_l1 + '_' + folder_l2 + '_' + band_label + '_ref_sza_vza_variation.npy'), [v_misr_sr_record, v_ahi_toa_record, v_ahi_sr_record])
+            # mapping
+            box_plot(numpy.array([v_misr_sr_record, v_ahi_toa_record, v_ahi_sr_record]))
