f12fd09f6df04d5fc8b282427503c036884b8dbc
ZHANG Beichen <45487945+Bosh0113@users.noreply.github.com>
2023-07-20 23:33:34 +0900

    RAA-Ray对比图加上LSR

---
+++ AHI-MISR_Inter-com_case/statistics_Ray_RAA_comp/00_Month_sr_slope_r_rmse_offset.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[56]:
+
+
+import os
+import numpy
+import re
+import random
+from scipy.stats import gaussian_kde, pearsonr
+from sklearn.metrics import mean_squared_error, r2_score
+import math
+import matplotlib.pyplot as plt
+
+WORK_SPACE = os.getcwd()
+
+PIXEL_PAIRS_MAX = 500
+
+MONTH_LABEL = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+def identifer(data):
+    down,up = numpy.nanpercentile(data,[0,75])
+    IQR = up-down
+    lower_limit = down - 1.5*IQR
+    upper_limit = up + 1.5*IQR
+    result = numpy.where(data > upper_limit,numpy.nan, data)
+    result = numpy.where(result < lower_limit,numpy.nan, result)
+    return result
+
+
+def mapping_scatter(Y, X, figure_title='demo', band_name='band3', axis_min=0.0, axis_max=0.8):
+    # filter
+
+    if band_name == 'band3':
+        axis_max = 0.6
+
+    lim_x = numpy.copy(X)
+    lim_y = numpy.copy(Y)
+
+    up4_x = numpy.quantile(lim_x, 0.98, interpolation='higher')
+    up4_y = numpy.quantile(lim_y, 0.98, interpolation='higher')
+    lim_x[lim_x > up4_x] = numpy.nan
+    lim_y[lim_y > up4_y] = numpy.nan
+
+    lim_mask = (lim_x*lim_y)*0+1
+    temp_x = lim_x*lim_mask
+    temp_y = lim_y*lim_mask
+    temp_x = temp_x[~numpy.isnan(temp_x)]
+    temp_y = temp_y[~numpy.isnan(temp_y)]
+
+    diff_array = abs(numpy.array(temp_y)-numpy.array(temp_x))/abs(numpy.minimum(numpy.array(temp_x), numpy.array(temp_y)))
+    diff_array_filtered = numpy.array(identifer(diff_array))
+    show_x = (diff_array_filtered*0+1)*temp_x
+    show_y = (diff_array_filtered*0+1)*temp_y
+    X = show_x[~numpy.isnan(show_x)]
+    Y = show_y[~numpy.isnan(show_y)]
+    
+    # Calculate the point density
+    xy = numpy.vstack([X, Y])
+    z = gaussian_kde(xy)(xy)
+    idx = z.argsort()
+    X, Y, z = X[idx], Y[idx], z[idx]
+    
+    # main value
+    max_n_idx = idx[-100:]
+    sr = round((X[max_n_idx].mean() + Y[max_n_idx].mean())/2, 3)
+    
+    
+    k, b = numpy.polyfit(X, Y, deg=1)
+    rmse = math.sqrt(mean_squared_error(X, Y))
+    r_, p = pearsonr(X, Y)
+
+    # slope r RMSE
+    return sr, k, r_, rmse, b
+
+
+# In[61]:
+
+
+if __name__ == "__main__":
+
+    folder_type_list = ['RAA', 'Ray']
+    folder_l1_list = ['26']
+    folder_l2_list = ['0']
+
+    for folder_type in folder_type_list:
+        print(folder_type)
+        folder_type_path = os.path.join(WORK_SPACE, folder_type)
+        for folder_l1 in folder_l1_list:
+            folder_l1_path = os.path.join(folder_type_path, folder_l1)
+            month_sr_b3 = numpy.zeros((12,))
+            month_slope_b3 = numpy.zeros((12,))
+            month_r_b3 = numpy.zeros((12,))
+            month_rmse_b3 = numpy.zeros((12,))
+            month_offset_b3 = numpy.zeros((12,))
+            month_sr_b4 = numpy.zeros((12,))
+            month_slope_b4 = numpy.zeros((12,))
+            month_r_b4 = numpy.zeros((12,))
+            month_rmse_b4 = numpy.zeros((12,))
+            month_offset_b4 = numpy.zeros((12,))
+            for month_idx in range(len(MONTH_LABEL)):
+                month = MONTH_LABEL[month_idx]
+                # each png
+                misr_SR_band3_item_list = []
+                ahi_SR_band3_item_list = []
+                misr_SR_band4_item_list = []
+                ahi_SR_band4_item_list = []
+                for folder_l2 in folder_l2_list:
+                    folder_l2_path = os.path.join(folder_l1_path, folder_l2)
+                    roi_folder_list = os.listdir(folder_l2_path)
+                    for roi_folder in roi_folder_list:
+                        roi_infos = roi_folder.split('_')
+                        roi_lat = float(roi_infos[2])
+                        
+                        if roi_lat < 0:
+                            roi_folder_path = os.path.join(folder_l2_path, roi_folder)
+                            roi_file_list = os.listdir(roi_folder_path)
+                            roi_misr_SR_band3_list = []
+                            roi_ahi_SR_band3_list = []
+                            roi_misr_SR_band4_list = []
+                            roi_ahi_SR_band4_list = []
+                            for roi_file in roi_file_list:
+                                matchObj = re.search(r'(\d+)_band(\d+)_(\d+).npy', str(roi_file))
+                                if matchObj:
+                                    ahi_time_str = matchObj.group(1)
+                                    band_str = matchObj.group(2)
+                                    # camera_idx_str = matchObj.group(3)
+
+                                    obs_month = ahi_time_str[4:6]
+                                    obs_month_idx = int(obs_month) - 1
+
+                                    if obs_month_idx == month_idx:
+                                        SR_npy_path = os.path.join(roi_folder_path, roi_file)
+                                        ROI_SR_pair = numpy.load(SR_npy_path, allow_pickle=True)[0]
+                                        misr_sr = ROI_SR_pair['misr_v3']
+                                        ahi_sr = ROI_SR_pair['ahi_sr2misr']
+                                        x_3Darray_np_1d = misr_sr.flatten()
+                                        x_3Darray_np_1d = x_3Darray_np_1d[~numpy.isnan(x_3Darray_np_1d)]
+                                        y_3Darray_np_1d = ahi_sr.flatten()
+                                        y_3Darray_np_1d = y_3Darray_np_1d[~numpy.isnan(y_3Darray_np_1d)]
+                                        if band_str == '3':
+                                            roi_misr_SR_band3_list.extend(x_3Darray_np_1d)
+                                            roi_ahi_SR_band3_list.extend(y_3Darray_np_1d)
+                                        if band_str == '4':
+                                            roi_misr_SR_band4_list.extend(x_3Darray_np_1d)
+                                            roi_ahi_SR_band4_list.extend(y_3Darray_np_1d)
+                                    # keep pixel count same
+                                    if len(roi_misr_SR_band3_list) == len(roi_misr_SR_band4_list):
+                                        misr_SR_band3_item_list.extend(roi_misr_SR_band3_list)
+                                        ahi_SR_band3_item_list.extend(roi_ahi_SR_band3_list)
+                                        misr_SR_band4_item_list.extend(roi_misr_SR_band4_list)
+                                        ahi_SR_band4_item_list.extend(roi_ahi_SR_band4_list)
+
+#                 print('Random NO.:', PIXEL_PAIRS_MAX)
+#                 print(folder_l1)
+#                 print('MISR_SR_Band3_NO.', 'AHI_SR_Band3_NO.', 'MISR_SR_Band4_NO.', 'AHI_SR_Band4_NO.')
+#                 print(len(misr_SR_band3_item_list), len(ahi_SR_band3_item_list), len(misr_SR_band4_item_list), len(ahi_SR_band4_item_list))
+
+                if len(misr_SR_band3_item_list) > PIXEL_PAIRS_MAX:
+                    # random pairs mapping
+                    index_array = random.sample([idx for idx in range(len(misr_SR_band3_item_list))], PIXEL_PAIRS_MAX)
+                    index_array = numpy.sort(index_array).tolist()
+
+                    misr_SR_band3_pts = numpy.array(misr_SR_band3_item_list)
+                    show_misr_sr_b3 = misr_SR_band3_pts[index_array]
+                    ahi_SR_band3_pts = numpy.array(ahi_SR_band3_item_list)
+                    show_ahi_sr_b3 = ahi_SR_band3_pts[index_array]
+                    figure_title = folder_l1 + '_b3' + '_' + str(month_idx) + month + '_' + str(PIXEL_PAIRS_MAX)
+                    sr_b3, slope_b3, r_b3, rmse_b3, offset_b3 = mapping_scatter(show_ahi_sr_b3, show_misr_sr_b3, figure_title, 'band3')
+                    month_sr_b3[month_idx] = sr_b3
+                    month_slope_b3[month_idx] = round(slope_b3, 2)
+                    month_r_b3[month_idx] = round(r_b3, 2)
+                    month_rmse_b3[month_idx] = round(rmse_b3, 3)
+                    month_offset_b3[month_idx] = round(offset_b3, 2)
+
+                    misr_SR_band4_pts = numpy.array(misr_SR_band4_item_list)
+                    show_misr_sr_b4 = misr_SR_band4_pts[index_array]
+                    ahi_SR_band4_pts = numpy.array(ahi_SR_band4_item_list)
+                    show_ahi_sr_b4 = ahi_SR_band4_pts[index_array]
+                    figure_title = folder_l1 + '_b4' + '_' + str(month_idx) + month + '_' + str(PIXEL_PAIRS_MAX)
+                    sr_b4, slope_b4, r_b4, rmse_b4, offset_b4 = mapping_scatter(show_ahi_sr_b4, show_misr_sr_b4, figure_title, 'band4')
+                    month_sr_b4[month_idx] = sr_b4
+                    month_slope_b4[month_idx] = round(slope_b4, 2)
+                    month_r_b4[month_idx] = round(r_b4, 2)
+                    month_rmse_b4[month_idx] = round(rmse_b4, 3)
+                    month_offset_b4[month_idx] = round(offset_b4, 2)
+                
+#                 else:
+#                     # all pairs mapping
+#                     pairs_no = len(misr_SR_band3_item_list)
+#                     if pairs_no > 3:
+
+#                         misr_SR_band3_pts = numpy.array(misr_SR_band3_item_list)
+#                         ahi_SR_band3_pts = numpy.array(ahi_SR_band3_item_list)
+#                         figure_title = folder_l1 + '_b3' + '_' + str(month_idx) + month + '_' + str(pairs_no)
+#                         sr_b3, slope_b3, r_b3, rmse_b3, offset_b3 = mapping_scatter(ahi_SR_band3_pts, misr_SR_band3_pts, figure_title, 'band3')
+#                         month_sr_b3[month_idx] = sr_b3
+#                         month_slope_b3[month_idx] = round(slope_b3, 2)
+#                         month_r_b3[month_idx] = round(r_b3, 2)
+#                         month_rmse_b3[month_idx] = round(rmse_b3, 3)
+#                         month_offset_b3[month_idx] = round(offset_b3, 2)
+
+#                         misr_SR_band4_pts = numpy.array(misr_SR_band4_item_list)
+#                         ahi_SR_band4_pts = numpy.array(ahi_SR_band4_item_list)
+#                         figure_title = folder_l1 + '_b4' + '_' + str(month_idx) + month + '_' + str(pairs_no)
+#                         sr_b4, slope_b4, r_b4, rmse_b4, offset_b4 = mapping_scatter(ahi_SR_band4_pts, misr_SR_band4_pts, figure_title, 'band4')
+#                         month_sr_b4[month_idx] = sr_b4
+#                         month_slope_b4[month_idx] = round(slope_b4, 2)
+#                         month_r_b4[month_idx] = round(r_b4, 2)
+#                         month_rmse_b4[month_idx] = round(rmse_b4, 3)
+#                         month_offset_b4[month_idx] = round(offset_b4, 2)
+            print('SR, Slope, r, RMSE, Offset')
+            print('Band3')
+
+            print(month_sr_b3)
+            print(month_slope_b3)
+            print(month_r_b3)
+            print(month_rmse_b3)
+            print(month_offset_b3)
+
+            print('Band4')
+            print(month_sr_b4)
+            print(month_slope_b4)
+            print(month_r_b4)
+            print(month_rmse_b4)
+            print(month_offset_b4)
+
+            sr_list = [month_sr_b3, month_sr_b4]
+            slope_list = [month_slope_b3, month_slope_b4]
+            r_list = [month_r_b3, month_r_b4]
+            rmse_list = [month_rmse_b3, month_rmse_b4]
+            offest_list = [month_offset_b3, month_offset_b4]
+
+            numpy.save(os.path.join(WORK_SPACE, folder_l1 + '_' + folder_type + '_sr_slope_r_rmse_offset.npy'), [sr_list, slope_list, r_list, rmse_list, offest_list])
+
--- AHI-MISR_Inter-com_case/statistics_Ray_RAA_comp/demo/RAA_Ray_month_comp.py
+++ AHI-MISR_Inter-com_case/statistics_Ray_RAA_comp/demo/RAA_Ray_month_comp.py
@@ -12,30 +12,42 @@ PIXEL_PAIRS_MAX = 500
 MONTH_LABEL = ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
 
 
-def display_pts_b3_b4(slope_list, r_list, rmse_list, match_type):
+def display_pts_b3_b4(sr_list, slope_list, r_list, rmse_list, match_type):
     plt.figure(figsize=(8,6))
     ax = plt.axes()
     ax.grid(linestyle='--', linewidth=0.3)
-    ax.set_title(match_type + '-matches', loc='left', fontstyle='oblique', fontsize='medium')
+#     ax.set_title(match_type + '-matches', loc='left', fontstyle='oblique', fontsize='medium')
 
     color_s = ['#FF4500', '#8B0000']
-    
+    # no-data mask
+    f_temp = sr_list[0]
+    f_temp = numpy.insert(f_temp, [0, len(f_temp)], [f_temp[-1:], f_temp[0]])
+    if match_type == 'RAA':
+        for sr_idx in range(len(f_temp)):
+            if f_temp[sr_idx] <= 0:
+                ax.axvline(sr_idx-1, color='whitesmoke', linewidth=50.0)
+    # display values
     band_labels = ['Band3', 'Band4']
     for idx in range(len(band_labels)):
         band_label = band_labels[idx]
+        
+        sr_month = sr_list[idx]
+        sr_month[sr_month <= 0] = numpy.nan
+        sr_month = numpy.insert(sr_month, [0, len(sr_month)], [sr_month[-1:], sr_month[0]])
+        
         slope_month = slope_list[idx]
-        slope_month[slope_month <= 0] = -10
+        slope_month[slope_month <= 0] = numpy.nan
         r_month = r_list[idx]
+        r_month[r_month <= 0] = numpy.nan
         rmse_month = rmse_list[idx]
-        rmse_month[rmse_month <= 0] = -10
-        # print(match_type, band_label)
-        # print(slope_month)
-        # print(r_month)
-        # print(rmse_month)
+        rmse_month[rmse_month <= 0] = numpy.nan
+        
 
+        ax.plot([i for i in range(-1,13,1)], sr_month, '-', color=color_s[idx], label=band_label + '_LSR')
         ax.plot([i for i in range(12)], slope_month, '1', color=color_s[idx], label=band_label + '_Slope', markersize=20)
         ax.plot([i for i in range(12)], r_month, '_', color=color_s[idx], label=band_label + '_r', markersize=10)
         ax.plot([i for i in range(12)], rmse_month, 'x', color=color_s[idx], label=band_label + '_RMSE', markersize=5)
+    
 
     ax.minorticks_on()
     x_minor_locator = plt.MultipleLocator(1)
@@ -56,8 +68,8 @@ def display_pts_b3_b4(slope_list, r_list, rmse_list, match_type):
     plt.xlim((-0.5, 11.5))
     plt.ylim((0.0, 1.08))
     plt.xlabel('Month', size=14)
-    plt.ylabel('AHI-MISR LSR Slope, r & RMSE', size=14)
-    plt.legend(markerscale=0.5, loc=10, bbox_to_anchor=[0.5, 0.25], ncol=2)
+    plt.ylabel('AHI-MISR LSR, Slope, r & RMSE', size=14)
+    plt.legend(markerscale=0.5, loc=10, bbox_to_anchor=[0.5, 0.50], ncol=2)
     plt.savefig(WORK_SPACE + '/VZA26_Ray_month.png', dpi=600)
     # plt.savefig(WORK_SPACE + '/VZA26_RAA_month.png', dpi=600)
     # plt.show()
@@ -65,15 +77,22 @@ def display_pts_b3_b4(slope_list, r_list, rmse_list, match_type):
 
 
 def show_para(mon_para, match_type):
-    slope_list = mon_para[0]
-    r_list = mon_para[1]
-    rmse_list = mon_para[2]
-    display_pts_b3_b4(slope_list, r_list, rmse_list, match_type)
+    sr_list = mon_para[0]
+    slope_list = mon_para[1]
+    r_list = mon_para[2]
+    rmse_list = mon_para[3]
+    
+    sr_w2w = numpy.hstack((sr_list[:, 6:], sr_list[:, :6]))
+    slope_w2w = numpy.hstack((slope_list[:, 6:], slope_list[:, :6]))
+    r_w2w = numpy.hstack((r_list[:, 6:], r_list[:, :6]))
+    rmse_w2w = numpy.hstack((rmse_list[:, 6:], rmse_list[:, :6]))
+    
+    display_pts_b3_b4(sr_w2w, slope_w2w, r_w2w, rmse_w2w, match_type)
 
 
 if __name__ == "__main__":
-    ray_month_para_npy = os.path.join(WORK_SPACE, '26_Ray_slope_r_rmse_offset.npy')
-    raa_month_para_npy = os.path.join(WORK_SPACE, '26_RAA_slope_r_rmse_offset.npy')
+    ray_month_para_npy = os.path.join(WORK_SPACE, '26_Ray_sr_slope_r_rmse_offset.npy')
+    raa_month_para_npy = os.path.join(WORK_SPACE, '26_RAA_sr_slope_r_rmse_offset.npy')
 
     ray_month_para = numpy.load(ray_month_para_npy, allow_pickle=True)
     raa_month_para = numpy.load(raa_month_para_npy, allow_pickle=True)
